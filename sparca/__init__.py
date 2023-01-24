071bc9158f18e4c0d475b57a345c12a2e2499331
Leland Barnard <leland.barnard@gmail.com>
2023-01-24 14:08:47 -0600

    added sparca to imports

--- sparca/__init__.py
+++ sparca/__init__.py
@@ -1,9 +1,10 @@
 from ._version import __version__ as _version
 from ._horn_selection import horn_selection
+from _sparca import SparCA
 
 __version__ = _version
 
 __all__ = [
     'horn_selection',
-    
+    'SparCA'
 ]
