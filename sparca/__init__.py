b22fc97768eabb90ffa9e6f3ce4ac305c19b65ae
Leland Barnard <leland.barnard@gmail.com>
2023-01-25 11:58:14 -0600

    load_test_data to init

--- sparca/__init__.py
+++ sparca/__init__.py
@@ -1,10 +1,12 @@
 from ._version import __version__ as _version
 from ._horn_selection import horn_selection
 from ._sparca import SparCA
+from ._load_test_data import load_test_data
 
 __version__ = _version
 
 __all__ = [
     'horn_selection',
-    'SparCA'
+    'SparCA',
+    'load_test_data'
 ]
