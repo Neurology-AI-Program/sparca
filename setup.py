01590eb86e80e4df2e5c41bc039ab12b6618e7e9
Leland Barnard <leland.barnard@gmail.com>
2023-01-25 11:20:52 -0600

    added test data

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     name = 'sparca.py',
     packages = setuptools.find_packages(),
     author = 'Leland Barnard',
-    version = __version__
+    version = __version__,
+    include_package_data=True,
+    package_data = {'sparca': ['test_data/*']}
 )
---
+++ sparca/_load_test_data.py
@@ -0,0 +1,21 @@
+import importlib
+import pandas as pd
+import json
+
+
+def load_test_data():
+
+    with importlib.resources.path('sparca', 'test_data') as data_root:
+        data_path = f'{data_root}/sparca_test_data.csv'
+        json_path = f'{data_root}/sparca_test_data_info.json'
+
+    test_data = pd.read_csv(data_path)
+
+    with open(json_path) as info_json:
+        data_info = json.load(info_json)
+
+    return test_data, data_info
+
+    
+
+    
