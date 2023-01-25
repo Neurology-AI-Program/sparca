ed5ca18ee527569f12e296c11219e494d3d3cc3c
Leland Barnard <leland.barnard@gmail.com>
2023-01-25 11:57:03 -0600

    smaller test data

--- sparca/_load_test_data.py
+++ sparca/_load_test_data.py
@@ -6,7 +6,7 @@ import json
 def load_test_data():
 
     with importlib.resources.path('sparca', 'test_data') as data_root:
-        data_path = f'{data_root}/sparca_test_data.csv'
+        data_path = f'{data_root}/sparca_test_data_sample.csv'
         json_path = f'{data_root}/sparca_test_data_info.json'
 
     test_data = pd.read_csv(data_path)
